b95a82a362f3f86ed9e590b9a94c5a1f546b935a
Alejandro Loja <alejandro.loja@gmail.com>
2020-09-25 18:47:30 -0400

    Change color and start to optimize html for bootstrap

--- script.js
+++ script.js
@@ -8,11 +8,12 @@ $(document).ready(function () {
     event.preventDefault();
     // Value (or what is written by the client) in the input box
     const rmName = $("#rm-name").val();
+    const rmDesc = $("#rm-description").val();
     // using .is(":checked") will give boolean to see if box is checked
     const rmNatChkBx = $("#rm-natural").is(":checked");
     const rmArtChkBx = $("#rm-artificial").is(":checked");
-    console.log(rmName,rmNatChkBx,rmArtChkBx);
-    console.log($(".row"));
+    console.log(rmName,rmDesc,rmNatChkBx,rmArtChkBx);
+    // console.log($(".row"));
     // console.log($("#rm_name").text());
   });
 
