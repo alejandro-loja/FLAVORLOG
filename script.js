1cccb258f3cf40c99ce424dd585c8424a6f3e37e
Alejandro Loja <alejandro.loja@gmail.com>
2020-09-25 00:10:07 -0400

    Extract some form data with jQuery

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-console.log('hello there');
+$(document).ready(function () {
 
-$( "#entry-form" ).submit(function( event ) {
-  console.log('Submit');
-  event.preventDefault();
+
+  console.log('hello there');
+
+  $("#entry-form").submit(function (event) {
+    console.log('Submit');
+    event.preventDefault();
+    // Value (or what is written by the client) in the input box
+    const rmName = $("#rm-name").val();
+    // using .is(":checked") will give boolean to see if box is checked
+    const rmNatChkBx = $("#rm-natural").is(":checked");
+    const rmArtChkBx = $("#rm-artificial").is(":checked");
+    console.log(rmName,rmNatChkBx,rmArtChkBx);
+    console.log($(".row"));
+    // console.log($("#rm_name").text());
+  });
+
+  // $(window).click(function() {
+  //   console.log($("#rm_name").text());
+  // })
+
 });
