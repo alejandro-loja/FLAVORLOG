e3a5b839fb75f7f101aeee242c0514e9cd500a22
Alejandro Loja <alejandro.loja@gmail.com>
2020-09-28 00:54:25 -0400

    Change to radio, take values

--- script.js
+++ script.js
@@ -9,6 +9,21 @@ $(document).ready(function () {
   }
   console.log('hello there');
 
+  function getRadioVal(form, name) {
+    var val;
+    // get list of radio buttons with specified name
+    var radios = form.elements[name];
+
+    // loop through list of radio buttons
+    for (var i = 0, len = radios.length; i < len; i++) {
+      if (radios[i].checked) { // radio checked?
+        val = radios[i].value; // if so, hold its value in val
+        break; // and break out of for loop
+      }
+    }
+    return val; // return value of checked radio or undefined if none checked
+  }
+
   $("#entry-form").submit(function (event) {
     console.log('Submit');
     event.preventDefault();
@@ -16,15 +31,16 @@ $(document).ready(function () {
     const rmName = $("#rm-name").val();
     const rmDesc = $("#rm-description").val();
     // using .is(":checked") will give boolean to see if box is checked
-    const rmNatChkBx = $("#rm-natural").is(":checked");
-    const rmArtChkBx = $("#rm-artificial").is(":checked");
+    // const rmNatChkBx = $("#rm-natural").is(":checked");
+    // const rmArtChkBx = $("#rm-artificial").is(":checked");
     const rmDosNum = $("#rm-dosage-number").val();
     const rmDosUnit = $("#rm-dosage-unit").val();
-    console.log(rmName, rmDesc, rmNatChkBx, rmArtChkBx, rmDosNum, rmDosUnit);
+    console.log(rmName, rmDesc, rmDosNum, rmDosUnit);
 
+    //fixe validates radio buttons
+    const val = getRadioVal(document.getElementById('entry-form'), 'rm-na');
+    console.log(val)
     validateForm(rmName);
   });
-
-
 
 });
