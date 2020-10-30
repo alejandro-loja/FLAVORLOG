a387695deb199821078a8c87ccfd0a7808a0e46d
Alejandro Loja <alejandro.loja@gmail.com>
2020-10-30 00:40:51 -0400

    Change bootstrap to add data entry, and GET entries

--- controllers/flavorlog_controller.js
+++ controllers/flavorlog_controller.js
@@ -1,34 +1,20 @@
 // This is where all the pages are made
 const express = require('express');
-
 const flavorlog = require("../models/flavorlog.js");
-
 const router = express.Router();
 
 // for __dirname
 const path = require('path');
 
 console.log("------");
-
-
-// homepage
-// router.get('/', function(req, res) {
-//   console.log('Went Home')
-//   res.sendFile(path.join(__dirname, "..", "/views/index.html"));
-
-// });
 router.get("/", function (req, res) {
-    res.render("index");
+    res.redirect("/results");
 });
 
 router.get('/results', function(req, res) {
-  flavorlog.all(function (data) {
-      var hasFlavorlog = {
-        flavorlogKinds: data
-      };
-      console.log(hasFlavorlog);
-      res.sendFile(path.join(__dirname, "..", "/views/results.html"));
-    });
+  flavorlog.all(function (flavorData) {
+    res.render("index", {flavor_data: flavorData});
+  });
 
 });
 
