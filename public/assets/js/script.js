c70c1cc6797e73542a5f5bbfdaa908948ab012ab
Alejandro Loja <alejandro.loja@gmail.com>
2020-12-15 22:30:49 -0500

    Dry up script.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,29 +1,53 @@
 $(document).ready(function () {
-  // Tells me is the input is a number or not.
-  const isThisNum = (number) => {
-    return isNaN(number) ? false : true;
+
+  const stnVal = {
+    isThisNum: function (number) {
+      return isNaN(number) ? false : true;
+    },
+    getRadioVal: function (form, name) {
+      // get list of radio buttons with specified name
+      const radios = form.elements[name];
+      // loop through list of radio buttons
+      for (var i = 0, len = radios.length; i < len; i++) {
+        if (radios[i].checked) { // radio checked?
+          return radios[i].value; // if so, hold its value in val
+        }
+      };
+    },
+    maxStringLength: function (number, stringLength) {
+      return number.toString().length > stringLength ?
+        // console.log(`${number} is too long. Please reduce the length`)
+        false :
+        // console.log(`${number} length is OK.`)
+        true;
+    }
   };
 
-  //This function tells you the value of the selected radio button
-  // You need the id of the form and name of the radio buttons
-  const getRadioVal = (form, name) => {
-    // get list of radio buttons with specified name
-    const radios = form.elements[name];
-    // loop through list of radio buttons
-    for (var i = 0, len = radios.length; i < len; i++) {
-      if (radios[i].checked) { // radio checked?
-        return radios[i].value; // if so, hold its value in val
-      }
-    };
-  };
+  // // Tells me is the input is a number or not.
+  // const isThisNum = (number) => {
+  //   return isNaN(number) ? false : true;
+  // };
 
-  const maxStringLength = (number, stringLength) => {
-    return number.toString().length > stringLength ?
-      // console.log(`${number} is too long. Please reduce the length`)
-      false :
-      // console.log(`${number} length is OK.`)
-      true;
-  };
+  // //This function tells you the value of the selected radio button
+  // // You need the id of the form and name of the radio buttons
+  // const getRadioVal = (form, name) => {
+  //   // get list of radio buttons with specified name
+  //   const radios = form.elements[name];
+  //   // loop through list of radio buttons
+  //   for (var i = 0, len = radios.length; i < len; i++) {
+  //     if (radios[i].checked) { // radio checked?
+  //       return radios[i].value; // if so, hold its value in val
+  //     }
+  //   };
+  // };
+
+  // const maxStringLength = (number, stringLength) => {
+  //   return number.toString().length > stringLength ?
+  //     // console.log(`${number} is too long. Please reduce the length`)
+  //     false :
+  //     // console.log(`${number} length is OK.`)
+  //     true;
+  // };
 
   $("#entry-form").submit(function (event) {
     console.log('Submitted');
@@ -35,21 +59,23 @@ $(document).ready(function () {
     const rmDosNum = $("#rm-dosage-number").val();
     const rmDosUnit = $("#rm-dosage-unit").val();
     //Value of Radio Button Pressed
-    const rmNa = getRadioVal(document.getElementById('entry-form'), 'rm-na');
+    const rmNa = stnVal.getRadioVal(document.getElementById('entry-form'), 'rm-na');
     //fixes validates radio buttons
     // Form Validation (need for mySQL based on schema)
 
-    if (maxStringLength(rmName, 100) & isThisNum(rmDosNum) & maxStringLength(rmDosNum, 5) & maxStringLength(rmDesc, 250)) {
-      console.log('all are true')
+    if (stnVal.maxStringLength(rmName, 100)
+      & stnVal.isThisNum(rmDosNum)
+      & stnVal.maxStringLength(rmDosNum, 5)
+      & stnVal.maxStringLength(rmDesc, 250)) {
       //AJAX POST
       const newEntry = {
-        rmName: rmName,
-        rmNa: rmNa,
-        rmDosNum: rmDosNum,
-        rmDosUnit: rmDosUnit,
-        rmDesc: rmDesc
+        rmName,
+        rmNa,
+        rmDosNum,
+        rmDosUnit,
+        rmDesc
       };
-      console.log(newEntry);
+      
       // Send the POST request.
       $.ajax("/flavorlog/create", {
         type: "POST",
@@ -122,10 +148,10 @@ $(document).ready(function () {
         const { rm_name, rm_na, rm_dosage_number, rm_dosage_unit, rm_description } = response;
         console.log(response);
         $(".modal-title").text(`${rm_name} - ${rm_na}`);
-        $(".modal-body").html(`
-      <h6>Evaluated at ${rm_dosage_number}${rm_dosage_unit}</h6>
-      <p>${rm_description}</p>
-      `)
+        $(".modal-body").html(
+          `<h6>Evaluated at ${rm_dosage_number}${rm_dosage_unit}</h6>
+                <p contenteditable="true">${rm_description}</p>`
+        )
       }
     );
 
